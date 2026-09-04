882647a0e7f03a39564c5400f7be9728e539e397
azkaprawira501-sys <azkaprawira501@gmail.com>
2026-09-05 00:56:20 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
 
+import 'package:flutter/material.dart';
+import 'config/api_config.dart';
+import 'views/home_view.dart';
+import 'views/login_view.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final loggedIn = await ApiConfig.isLoggedIn();
+
+  runApp(
+    MaterialApp(
+      title: 'SDIT Piket',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(useMaterial3: true),
+      home: loggedIn ? const HomeView() : const LoginView(),
+    ),
+  );
+}
