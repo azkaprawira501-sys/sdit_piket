c3bb271e20d5a0fa988f2695b2a1b65790346126
azkaprawira501-sys <azkaprawira501@gmail.com>
2026-09-05 00:58:14 +0800

    Update api_config.dart

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,1 +1,51 @@
 
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ApiConfig {
+  static const String keyBaseUrl = 'base_url';
+  static const String keyUserName = 'user_name';
+  static const String keyUserId = 'user_id';
+  static const String keyIsLoggedIn = 'is_logged_in';
+  static const String defaultUrl = 'http://192.168.1.15:8000';
+
+  static Future<String> getBaseUrl() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(keyBaseUrl) ?? defaultUrl;
+  }
+
+  static Future<void> setBaseUrl(String url) async {
+    final prefs = await SharedPreferences.getInstance();
+    String cleanUrl = url.trim();
+    if (cleanUrl.endsWith('/')) {
+      cleanUrl = cleanUrl.substring(0, cleanUrl.length - 1);
+    }
+    await prefs.setString(keyBaseUrl, cleanUrl);
+  }
+
+  static Future<bool> isLoggedIn() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(keyIsLoggedIn) ?? false;
+  }
+
+  static Future<void> saveUser(int id, String name) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(keyUserId, id);
+    await prefs.setString(keyUserName, name);
+    await prefs.setBool(keyIsLoggedIn, true);
+  }
+
+  static Future<Map<String, dynamic>> getUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    return {
+      'id': prefs.getInt(keyUserId) ?? 0,
+      'name': prefs.getString(keyUserName) ?? 'Guru Piket',
+    };
+  }
+
+  static Future<void> logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(keyIsLoggedIn);
+    await prefs.remove(keyUserId);
+    await prefs.remove(keyUserName);
+  }
+}
