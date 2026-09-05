a63b9e8542518386716a018ae36386e9e7a87838
azkaprawira501-sys <azkaprawira501@gmail.com>
2026-09-05 09:55:27 +0800

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,1 +1,9 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../config/firebase_config.dart';
 
+class ApiService {
+  static Future<Map<String, dynamic>> login(String email, String password) async {
+    return {'success': false, 'message': 'Gunakan Firebase Auth'};
+  }
+}
