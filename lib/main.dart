d9407abeec47a3d23d20b80400941d7023389749
azkaprawira501-sys <azkaprawira501@gmail.com>
2026-09-05 08:20:45 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
-
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'config/api_config.dart';
 import 'views/home_view.dart';
 import 'views/login_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Inisialisasi Firebase Engine
+  try {
+    await Firebase.initializeApp();
+  } catch (e) {
+    debugPrint('Firebase auto-init: $e');
+  }
+
   final loggedIn = await ApiConfig.isLoggedIn();
 
   runApp(
